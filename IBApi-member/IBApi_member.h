ec193274ddf307df997b85975eabdf4793634034
c0710204 <c0710204@gmail.com>
2013-05-11 01:36:42 +0800

    Initial Commit

---
+++ IBApi-member/IBApi_member.h
@@ -0,0 +1,13 @@
+//
+//  IBApi_member.h
+//  IBApi-member
+//
+//  Created by Apple on 13-5-11.
+//  Copyright (c) 2013年 thephoenixorg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IBApi_member : NSObject
+
+@end
---
+++ IBApi-member/IBApi_member.m
@@ -0,0 +1,13 @@
+//
+//  IBApi_member.m
+//  IBApi-member
+//
+//  Created by Apple on 13-5-11.
+//  Copyright (c) 2013年 thephoenixorg. All rights reserved.
+//
+
+#import "IBApi_member.h"
+
+@implementation IBApi_member
+
+@end
